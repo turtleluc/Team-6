5f47051529af072ddf9efd0cab13f3b32fd056f3
turtleluc <turtleluczuidema@gmail.com>
2023-01-09 13:23:11 +0100

    Button script werkt niet mee

---
+++ Assets/Scripts/Test.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Test : MonoBehaviour
+{
+    public int k = 1
+    
+    public void Teller()
+    {
+        k++;
+        Debug.Log(k)
+        Debug.Log("test")
+    }
+
+    
+}
