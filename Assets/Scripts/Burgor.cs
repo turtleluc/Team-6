32b5a12b5cbf7598f6d37daf3a5bebed4fe65ea1
lucauil <119048381+lucauil@users.noreply.github.com>
2023-01-12 14:36:05 +0100

    text script aangepast

--- Assets/Scripts/Burgor.cs
+++ Assets/Scripts/Burgor.cs
@@ -1,17 +1,16 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.UI;
 
 public class Burgor : MonoBehaviour
 {
 
-    public Sprite newButtonImage;
-    public Button newButton;
+    
 
     public void Disable()
     {
-      newButton.image.sprite = newButtonImage;
+        boton2 = GetComponent<Button>();
+        boton2.interactable = false;
     }
 
     
