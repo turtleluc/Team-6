65eab4c5e7d58e00cf86abac6910633606349f5d
lucauil <119048381+lucauil@users.noreply.github.com>
2023-01-16 20:57:01 +0100

    en hier is het script wat erbij hoort

--- Assets/Scripts/InputManager.cs
+++ Assets/Scripts/InputManager.cs
@@ -1,18 +1,40 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class InputManager : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    private TouchControls touchControls;
+
+    private void Awake()
     {
-        
+        touchControls = new TouchControls();
     }
 
-    // Update is called once per frame
-    void Update()
+    private void OnEnable()
     {
-        
+        touchControls.Enable();
+    }
+
+    private void OnDisable();
+    {
+        touchControls.Disable();
+    }
+
+    private void start()
+    {
+        touchControls.Touch.TouchPress.started += ctx => StartTouch(ctx);
+        touchControls.Touch.TouchPress.canceld += ctx => EndTouch(ctx);
+    }
+
+    private void StartTouch(InputAction.CallbackContext context)
+    {
+        Debug.log("Touch started" +touchControls.Touch.TouchPosition.ReadValue<Vector2>());
+    
+    }
+
+     private void EndTouch(InputAction.CallbackContext context)
+    {
+        Debug.log("Touch ended");
+    
     }
 }
